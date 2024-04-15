2e5b0f0dd36026d20d3b36f6f4df7287725dad25
Colon101 <kfirgoldman12@gmail.com>
2024-04-16 00:20:07 +0300

    Done

---
+++ src/lib/question.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let question: string;
+	export let result: number;
+	export let questionNum: number;
+</script>
+
+<div class="flex justify-center py-12">
+	<h1 class="text-4xl text-center">{question}</h1>
+</div>
+<div class="flex flex-col items-center gap-3">
+	{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as i}
+		<button
+			class=" rounded-md cursor-pointer max-w-[1000px] w-5/6 h-14 bg-gray-500 hover:bg-slate-700 hover:scale-[1.03] transition-all flex items-center"
+			on:click={() => {
+				questionNum += 1;
+				result += i;
+			}}
+		>
+			<span class="ml-4">
+				{i} {i === 1 ? 'Not true of me at all' : ''} {i === 10 ? 'Very true of me' : ''}</span
+			>
+		</button>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Question from '$lib/question.svelte';
+	let result = 0;
+	let questionIndex = 0;
+</script>
+
+<div class="pb-3">
+	{#if questionIndex === 0}
+		<Question
+			question={'1. I enjoy spending time in solitude.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 1}
+		<Question
+			question={'2. I prefer for someone else to be the center of attention.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 2}
+		<Question
+			question={'3. I prefer spending time one-on-one with others rather than hanging out in a group.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 3}
+		<Question
+			question={'4. I listen and think before I speak.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 4}
+		<Question
+			question={'5. I need time to process before making decisions.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 5}
+		<Question
+			question={'6. I’m drawn to deep conversations and thoughts.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 6}
+		<Question
+			question={'7. I need time alone to recharge and reflect.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 7}
+		<Question
+			question={'8. I’m observant and often notice what others miss.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 8}
+		<Question
+			question={'9. I prefer working in quiet, independent environments.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex === 9}
+		<Question
+			question={'10. I’m at my best when I fully focus rather than divide my attention.'}
+			bind:result
+			bind:questionNum={questionIndex}
+		/>
+	{/if}
+	{#if questionIndex > 9}
+		<h1 class="text-4xl text-center font-semibold">Your result: {result}/100</h1>
+	{/if}
+</div>
